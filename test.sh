36b2ebf0e2bb1990cb9b007cdc81261adf2b748e
Jiahaoli <55016725+liblacklucy@users.noreply.github.com>
2024-05-20 15:14:46 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 # VOC2012
-#CUDA_VISIBLE_DEVICES="6" python test.py configs/voc12/CrossPromptCLIP_seg_zero_vit-b_512x512_20k_12_10_debug2.py /data/ljh/segmentation/ZS3CLIP/output/voc12/cpt_debug2/best_U_mIoU_iter_14000.pth --eval=mIoU
+#CUDA_VISIBLE_DEVICES="6" python test.py configs/voc12/CrossPromptCLIP_seg_zero_vit-b_512x512_20k_12_10_debug2.py YOUR PTH --eval=mIoU
 
 # COCO
-CUDA_VISIBLE_DEVICES="2" python test.py configs/coco/CrossPromptCLIP_seg_zero_vit-b_512x512_80k_12_100_multi_debug8.py /data/ljh/segmentation/ZS3CLIP/output/coco/cpt_debug8/latest.pth --eval=mIoU
+CUDA_VISIBLE_DEVICES="2" python test.py configs/coco/CrossPromptCLIP_seg_zero_vit-b_512x512_80k_12_100_multi_debug8.py YOUR PTH --eval=mIoU
