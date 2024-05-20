b5a6270088fd3b60f0808e39886586e5845976ef
Jiahaoli <55016725+liblacklucy@users.noreply.github.com>
2024-05-20 15:14:01 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 # VOC2012
-bash dist_train.sh configs/voc12/CrossPromptCLIP_seg_zero_vit-b_512x512_20k_12_10_debug2.py /data/ljh/segmentation/ZS3CLIP/output/voc12/cpt_debug2
+bash dist_train.sh configs/voc12/CrossPromptCLIP_seg_zero_vit-b_512x512_20k_12_10_debug2.py /output/voc12/cpt_debug2
 
-# COC
-#bash dist_train.sh configs/coco/CrossPromptCLIP_seg_zero_vit-b_512x512_80k_12_100_multi_debug8.py /data/ljh/segmentation/ZS3CLIP/output/coco/cpt_debug8
+# COCO
+#bash dist_train.sh configs/coco/CrossPromptCLIP_seg_zero_vit-b_512x512_80k_12_100_multi_debug8.py /output/coco/cpt_debug8
